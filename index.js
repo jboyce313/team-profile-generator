ae8d7c83c64ebec0075dc6c15f1719e2d6066ea0
Jacob Boyce <jboyce313@gmail.com>
2023-02-20 16:33:17 -0600

    added functions for adding engineers and interns

--- index.js
+++ index.js
@@ -10,54 +10,56 @@ const team = {
   interns: [],
 };
 
-function addTeamMembers() {
-  let finished = false;
-  while (!finished) {
-    inquirer.prompt(questions.options).then((response) => {
-      switch (response.choice) {
-        case "Add engineer":
-          inquirer.prompt(questions.engineerQuestions).then((data) => {
-            team.engineers.push(
-              new Engineer(
-                data.engineerName,
-                data.engineerID,
-                data.engineerEmail,
-                data.engineerGithub
-              )
-            );
-          });
-          break;
-        case "Add intern":
-          inquirer.prompt(questions.internQuestions).then((data) => {
-            team.interns.push(
-              new Intern(
-                data.internName,
-                data.internID,
-                data.internEmail,
-                data.internSchool
-              )
-            );
-          });
-          break;
-        case "Finish building team":
-          finished = true;
-          break;
-      }
-    });
-  }
+function addManager(response) {
+  team.manager = new Manager(
+    response.managerName,
+    response.managerID,
+    response.managerEmail,
+    response.managerOfficeNumber
+  );
+}
+
+function addEngineer() {
+  return inquirer.prompt(questions.engineerQuestions).then((data) => {
+    team.engineers.push(
+      new Engineer(
+        data.engineerName,
+        data.engineerID,
+        data.engineerEmail,
+        data.engineerGithub
+      )
+    );
+  });
+}
+
+function addIntern() {
+  return inquirer.prompt(questions.internQuestions).then((data) => {
+    team.interns.push(
+      new Intern(
+        data.internName,
+        data.internID,
+        data.internEmail,
+        data.internSchool
+      )
+    );
+  });
 }
 
 function buildTeam() {
-  inquirer.prompt(questions.managerQuestions).then((response) => {
-    team.manager = new Manager(
-      response.managerName,
-      response.managerID,
-      response.managerEmail,
-      response.managerOfficeNumber
-    );
-    addTeamMembers();
-    console.log(team);
-  });
+  inquirer
+    .prompt(questions.managerQuestions)
+    .then((response) => addManager(response))
+    .then(() => inquirer.prompt(questions.options))
+    .then((response) => {
+      if (response.choice === "Add engineer") {
+        return addEngineer();
+      } else if (response.choice === "Add intern") {
+        return addIntern();
+      } else {
+        return;
+      }
+    })
+    .then(() => console.log(team));
 }
 
 buildTeam();
